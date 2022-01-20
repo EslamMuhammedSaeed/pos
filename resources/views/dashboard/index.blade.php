0197f9c25a6af355141fd1db8e1ec1344e6f78df
Eslam Mohammed Saeed <eslam.muhammed.saeed@gmail.com>
2022-01-20 11:55:45 +0200

    project setup commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.dashboard.app')
+
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>blank page <small>all starts here</small></h1>
+        </section>
+        <section class="content-header">
+            <h1>dashboard</h1>
+        </section>
+    </div>
+
+
+    
+@endsection
